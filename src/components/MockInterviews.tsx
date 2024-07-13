316a647f58843fae6501cc01efdd30a1cf5cad1d
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-07-13 21:55:16 +0530

    Refactor MockInterviews component to remove unused interview card
    Remove the unused interview card in the MockInterviews component to improve code cleanliness and reduce unnecessary rendering. This change enhances performance and ensures a more streamlined user experience.

--- src/components/MockInterviews.tsx
+++ src/components/MockInterviews.tsx
@@ -36,26 +36,6 @@ const MockInterviews = ({ jobId }: { jobId: string }) => {
                         </div>
                     </div>
                 </Link>
-                <Link href={`/dashboard/${jobId}/interview/1`} className='flex items-center m-2 bg-muted px-4 py-4 rounded-md border w-80 text-start'>
-                    <Image src="/mockinterview.jpg" alt="interview-question" height="80" width="80" className='rounded-md mr-4' />
-                    <div className='flex flex-col space-y-1'>
-                        <h3 className='text-xl text-semibold'>Interview 1</h3>
-                        <div className='flex gap-2'>
-                            <div className='flex items-center gap-1 text-xs'>
-                                <Calendar className='h-4 w-4' />
-                                03/07/2024
-                            </div>
-                            <div className='flex items-center gap-1 text-xs'>
-                                <Clock className='h-4 w-4' />
-                                10:11 a.m
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-2 text-xs'>
-                            <Check className='h-4 w-4' />
-                            Ready
-                        </div>
-                    </div>
-                </Link>
             </div>
         </div>
     )
