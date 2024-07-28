70d8d19592c479ababd7afde2372f796eb567bbe
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-07-28 12:35:21 +0530

    chore: Refactor transcribeaudio route to handle missing audio file

--- src/app/api/transcribeaudio/route.ts
+++ src/app/api/transcribeaudio/route.ts
@@ -4,8 +4,6 @@ import axios from 'axios';
 import { Storage } from '@google-cloud/storage';
 import { adminClient } from '@/utils/supabase/admin';
 
-export const maxDuration = 60;
-
 export async function POST(req: NextRequest) {
 
     try {
@@ -26,12 +24,14 @@ export async function POST(req: NextRequest) {
 
         const arrayBuffer = await audioBlob.arrayBuffer();
         const audioFileData = new Uint8Array(arrayBuffer);
+        console.log("File Uploading!");
         const uploadResponse = await axios.post(`https://api.assemblyai.com/v2/upload`, audioFileData, {
             "headers": {
                 "authorization": process.env.ASSEMBLYAI_API_KEY!,
                 'Content-Type': 'application/octet-stream',
             },
         })
+        console.log("File Uploaded!");
         const uploadUrl = uploadResponse.data.upload_url
 
         // const storage = new Storage({
@@ -51,17 +51,18 @@ export async function POST(req: NextRequest) {
         //     contentType: 'audio/mpeg',
         // });
 
-        // client.transcripts.submit({
-        //     audio: `https://storage.googleapis.com/prepiq_bucket_1/${fileName}`,
-        //     webhook_url: `${process.env.NEXT_PUBLIC_HOST}/webhook/speechtotext?interview_id=${interviewId}&question_id=${questionId}`,
-        //     webhook_auth_header_name: "Prepiq-Assembly-Webhook-Secret",
-        //     webhook_auth_header_value: process.env.ASSEMBLYAI_WEBHOOK_SECRET!
-        // })
-        const data = {
-            audio_url: uploadUrl
-        }
-        const transcript = await client.transcripts.transcribe(data);
-        await supabase.from('interview_questions').update({ submitted_answer: transcript.text }).eq('id', questionId).eq('interview_id', interviewId);
+        client.transcripts.submit({
+            audio: uploadUrl,
+            webhook_url: `${process.env.NEXT_PUBLIC_HOST}/api/webhook/speechtotext?interview_id=${interviewId}&question_id=${questionId}`,
+            webhook_auth_header_name: "Prepiq-Assembly-Webhook-Secret",
+            webhook_auth_header_value: process.env.ASSEMBLYAI_WEBHOOK_SECRET!
+        })
+        console.log("webhook submitted.")
+        // const data = {
+        //     audio_url: uploadUrl
+        // }
+        // const transcript = await client.transcripts.transcribe(data);
+        // await supabase.from('interview_questions').update({ submitted_answer: transcript.text }).eq('id', questionId).eq('interview_id', interviewId);
         return NextResponse.json({ success: true, message: 'File uploaded successfully' }, { status: 200 });
     } catch (error) {
         console.log('Error transcribing audio:', error)
