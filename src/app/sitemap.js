cca490071ac58140e658faf427da88cad6ab4321
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-07-28 22:24:30 +0530

    added sitemap and robot.txt

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default async function sitemap() {
+
+    const routes = ["", "/contact", "/privacypolicy", "/termsandcondition", 'sign-in'].map((route) => ({
+        url: `${process.env.NEXT_PUBLIC_HOST}${route}`,
+        lastModified: new Date().toISOString(),
+    }));
+
+
+    return [...routes];
+}
