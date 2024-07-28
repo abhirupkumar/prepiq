3cdb1a9868d5c7b9da87e4eae7379de2f8436ec9
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-07-28 12:07:04 +0530

    Refactor transcribeaudio route to handle missing audio file

--- src/app/api/transcribeaudio/route.ts
+++ src/app/api/transcribeaudio/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { AssemblyAI } from 'assemblyai';
 import { createClient } from '@/utils/supabase/server';
 import axios from 'axios';
+import { Storage } from '@google-cloud/storage';
 
 export const maxDuration = 60;
 
@@ -23,23 +24,41 @@ export async function POST(req: NextRequest) {
             apiKey: process.env.ASSEMBLYAI_API_KEY!,
         });
 
-        const arrayBuffer = await audioBlob.arrayBuffer();
-        const audioFileData = new Uint8Array(arrayBuffer);
-        const uploadResponse = await axios.post(`https://api.assemblyai.com/v2/upload`, audioFileData, {
-            "headers": {
-                "authorization": process.env.ASSEMBLYAI_API_KEY!,
-                'Content-Type': 'application/octet-stream',
-            },
-        })
-        const uploadUrl = uploadResponse.data.upload_url
+        // const arrayBuffer = await audioBlob.arrayBuffer();
+        // const audioFileData = new Uint8Array(arrayBuffer);
+        // const uploadResponse = await axios.post(`https://api.assemblyai.com/v2/upload`, audioFileData, {
+        //     "headers": {
+        //         "authorization": process.env.ASSEMBLYAI_API_KEY!,
+        //         'Content-Type': 'application/octet-stream',
+        //     },
+        // })
+        // const uploadUrl = uploadResponse.data.upload_url
+
+        const storage = new Storage({
+            projectId: process.env.GOOGLE_PROJECT_ID!,
+            credentials: {
+                client_email: process.env.GOOGLE_CLIENT_EMAIL!,
+                private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+            }
+        });
+
+        const fileName = `${Date.now()}_audio.mp3`;
+
+        const bucket = storage.bucket(process.env.GOOGLE_STORAGE_BUCKET!);
+        const file = bucket.file(fileName);
+
+        await file.save(Buffer.from(await audioBlob.arrayBuffer()), {
+            contentType: 'audio/mpeg',
+        });
+
         client.transcripts.submit({
-            audio: uploadUrl,
+            audio: `https://storage.googleapis.com/prepiq_bucket_1/${fileName}`,
             webhook_url: `${process.env.NEXT_PUBLIC_HOST}/webhook/speechtotext?interview_id=${interviewId}&question_id=${questionId}`,
             webhook_auth_header_name: "Prepiq-Assembly-Webhook-Secret",
             webhook_auth_header_value: process.env.ASSEMBLYAI_WEBHOOK_SECRET!
         })
         // const data = {
-        //     audio_url: uploadUrl
+        //     // audio_url: uploadUrl
         // }
         // const transcript = await client.transcripts.transcribe(data);
         // await supabase.from('interview_questions').update({ submitted_answer: transcript.text }).eq('id', questionId).eq('interview_id', interviewId);
