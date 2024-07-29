cd8e3bc2d584ae85f35bb88625987c481f72afe9
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2024-07-29 12:49:49 +0530

    chore: Update sitemap routes and robots.txt rules

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,20 +18,19 @@ export function constructMetadata({
   description = "PrepiQ is an Interview Prep SaaS application that gives you the most comprehensive interview preparation experience so that you can crush your next interview and land the next job offer.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
-  noIndex = false,
   keywords = ['prepiq', 'prep iq', 'interview preparation for experienced', 'ai', 'mock', 'job', 'naukri', 'taiyari', 'behavioural round', 'answering questions during an interview', 'common interview questions', 'aptitude', 'strengths', 'weakness', 'interview help', 'interview prep questions', 'job interview preparation', 'recruitment']
 }: {
   title?: string
   description?: string
   image?: string
   icons?: string
-  noIndex?: boolean,
   keywords?: string[]
 } = {}): Metadata {
   return {
     title,
     description,
     keywords,
+    authors: [{ name: 'ABHIRUP KUMAR BHOWMICK', url: 'https://abhirupkumar.vercel.app' }],
     openGraph: {
       title,
       description,
@@ -43,12 +42,13 @@ export function constructMetadata({
     },
     icons,
     metadataBase: new URL('https://prepiq.vercel.app'),
-    ...(noIndex && {
-      robots: {
-        index: false,
-        follow: false
-      }
-    }),
+    alternates: {
+      canonical: './',
+    },
+    robots: {
+      index: true,
+      follow: true
+    },
     other: {
       "google-site-verification": "co0B-CYrqRdI_fmi2ySiu2nYjJd2Wq8Db9ESsAv_uZE"
     }
